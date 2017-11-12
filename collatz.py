e99cb18a2ca792f0062afd4ab6ef8322d7b1076f
gaoyaoxin <gaoyaoxin@github.com>
2017-11-12 20:04:54 +0800

    update1112

---
+++ collatz.py
@@ -0,0 +1,13 @@
+def collatz(number):
+	if number % 2 == 0:
+		return number // 2
+	elif number % 2 == 1:
+		return 3 * number + 1
+
+print('Enter number:')
+inputNumber = int(input())
+while True:
+	print(collatz(inputNumber)) # 调用函数后，print()才可打印返回值
+	inputNumber = collatz(inputNumber) # 关键！
+	if inputNumber == 1:
+		break
---
+++ helloFunc2None.py
@@ -0,0 +1,5 @@
+def hello(name):
+	print('Hello ' + name)
+	
+spam = hello('Alice')
+print(None == spam)
